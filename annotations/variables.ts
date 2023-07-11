cb8a76d4469d21fb1b1788bd8dfd8efc811f1475
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-11 15:37:17 -0700

    went over variables

---
+++ annotations/variables.ts
@@ -0,0 +1,11 @@
+// colon tells TS that the variable apple will only ever use a number type
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+// these too have a type that is the same as the value
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
