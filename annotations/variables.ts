df511ecf799b265f0a85b7dda15263c696bc6148
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-11 15:43:36 -0700

    added notes on arrays classess and object literals

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -9,3 +9,18 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array; first brackets are not the array, they tell typescript that this value is going to be an array. Hence the second brackets with the colors inside
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [2, 245, 23];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object Literal; declaring a variable and assigning it an object with X and Y and we add type annotation that tells TS that X and Y have a type
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 23,
+};
