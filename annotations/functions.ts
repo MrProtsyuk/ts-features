636e0c4170c082a106c172fc26473749b8a14970
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-11 17:31:56 -0700

    func annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -7,3 +7,12 @@ const add = (a: number, b: number) => {
 const subtract = (a: number, b: number): number => {
   return a - b;
 };
+
+// no matter how you write your functions (examples below)
+// the annotations stay the same v
+function divide(a: number, b: number): number {
+  return a / b;
+}
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
