96f38a5eb236391bf78bf42e809427c2d5f711fd
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-11 17:29:09 -0700

    adding annotations so TS knows you need to return something in a func

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,4 +1,9 @@
 // basic function showing type annotations and how to work with them in the function
-const add = (a: number, b: number): number => {
+const add = (a: number, b: number) => {
   return a + b;
+};
+
+// sure to add the annoatation on the end of the function so typescript knows you need to return something!!!
+const subtract = (a: number, b: number): number => {
+  return a - b;
 };
