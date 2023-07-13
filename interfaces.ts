0d5431667008ab4a6c75dcf9363345b8b95afb96
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-12 21:25:09 -0700

    functions within interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -3,6 +3,8 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  // a function that returns a string
+  summary(): string;
 }
 
 // creating an example vehicle
@@ -10,6 +12,9 @@ const oldCivic = {
   name: "Civic",
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}, Year: ${this.year}, Broken: ${this.broken} `;
+  },
 };
 
 // intead of passing in all the different types, we can pass in our interface instead and it helps keep things clean
