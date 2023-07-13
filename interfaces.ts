8d9ac8a7f7c3e2448b8d3e7a26288bf070d379af
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-12 21:49:31 -0700

    wrapped up basics of interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -18,6 +18,15 @@ const oldCivic = {
   },
 };
 
+const soda = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `Coca Cola is ${this.color} and has ${this.sugar} grams of sugar in it.`;
+  },
+};
+
 // intead of passing in all the different types, we can pass in our interface instead and it helps keep things clean
 const printVehicle = (vehicle: Vehicle): void => {
   console.log(vehicle.summary());
@@ -26,6 +35,10 @@ const printVehicle = (vehicle: Vehicle): void => {
 printVehicle(oldCivic);
 
 // you could also make interfaces super generic
+// when you create a function within an interface you are able to create different objects that have no realtion what so ever
+// but you are able to call those objects within functions because you are passing that function in from the interface. Example here is
+// the function summary(): string; which can be found in both oldCivic and soda, in the printSummary function below I can pass either
+// and will not get an error because both contain summary() and satisfy typescripts interface.
 interface Reportable {
   summary(): string;
 }
@@ -34,4 +47,4 @@ const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
 
-printSummary(oldCivic);
+printSummary(soda);
