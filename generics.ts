cba310b4b848ffd85de6287a68a8372a5ce788e5
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-07-25 10:57:34 -0700

    added generics with functions

--- generics.ts
+++ generics.ts
@@ -23,3 +23,24 @@ class ArrayOfAnything<T> {
 }
 
 new ArrayOfAnything<string>([]);
+
+//Example of generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>([]);
